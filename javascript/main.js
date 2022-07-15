ea891a53a0dc396792802461b0056cb1cdaf89e4
Addisalem <addishailegnaw@yahoo.com>
2022-07-15 20:11:06 +0300

    If statements

--- javascript/main.js
+++ javascript/main.js
@@ -3,8 +3,8 @@ var myNum = 10;
 
 
 if (myNum == 10){
-    document.write("myNum is equal to 10")
+    document.write("myNum is equal to 10");
 }
 else{
-    document.write("myNum is not equal to 10")
+    document.write("myNum is not equal to 10");
 }

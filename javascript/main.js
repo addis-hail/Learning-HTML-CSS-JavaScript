bdd0907183f01f80c5ede06424194dd9fb56d27d
Addisalem <addishailegnaw@yahoo.com>
2022-07-16 22:16:19 +0300

    Variable Scope

--- javascript/main.js
+++ javascript/main.js
@@ -1,7 +1,12 @@
 function getAverage (a,b,c,d,e,f){
-    var average = (a + b + c + d + e + f)/2;
+    var average = (a + b + c + d + e + f)/2;//local variable
     console.log(average);
     return average;
 }
- var myResult = getAverage(28,12,88,99,11,10);
-console.log("The average is:" + myResult);
+ var myResult = getAverage(28,12,88,99,11,10);//global variable
+
+
+function logResult(){
+    console.log("The average is: " + myResult + " insde the functiom");
+}
+logResult();

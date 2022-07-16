e22c41a68aae8f7190ce92787edce9b40405658a
Addisalem <addishailegnaw@yahoo.com>
2022-07-16 22:04:17 +0300

    Functions

--- javascript/main.js
+++ javascript/main.js
@@ -1,5 +1,7 @@
-var links = document.getElementsByTagName("a");
-
-for(i = 0; i < links.length; i++){
-    links[i].className = "link" + i;
+function getAverage (a,b,c,d,e,f){
+    var average = (a + b + c + d + e + f)/2;
+    console.log(average);
+    return average;
 }
+ var myResult = getAverage(28,12,88,99,11,10);
+console.log("The average is:" + myResult);

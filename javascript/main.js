0b144545d6a3c2fde0d7362f3dfe2e488fe1565a
Addisalem <addishailegnaw@yahoo.com>
2022-07-17 11:20:58 +0300

    Creating new objects

--- javascript/main.js
+++ javascript/main.js
@@ -1,9 +1,27 @@
-var myString = "This is a \"fun\" string hate";
+var myArray = new Array();
+myArray[0] = 8;
+myArray[1] = "hello";
 
-if(myString.indexOf("hate") === -1){
-    console.log("The word hate is not in the string");
-}
-else{
-    console.log("The word hate starts at: " + myString.indexOf("hate"));
-}
+var myCar = new Object();
+myCar.maxSpeed = 50;
+myCar.driver = "Addisalem";
+
+myCar.drive = function(){
+    console.log("Now driving");
+};
+
+myCar.drive();
+
+var myCar2 = {
+    maxSpeed:70, 
+    driver:"Addisalem", 
+    drive:function(speed, time){
+        console.log(speed * time);
+    }
+};
+
+console.log(myCar2.maxSpeed);
+myCar2.drive(50, 3);
+
+
 

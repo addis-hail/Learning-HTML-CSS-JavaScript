9479f3212c3bcc99b8a5e91198e48fc2dbe6c386
Addisalem <addishailegnaw@yahoo.com>
2022-07-17 11:53:15 +0300

    Constructor functions

--- javascript/main.js
+++ javascript/main.js
@@ -1,16 +1,23 @@
-var myCar2 = {
-    maxSpeed:70, 
-    driver:"Addisalem", 
-    drive:function(speed, time){
+var Car = function(maxSpeed, driver){
+    this.maxSpeed = maxSpeed;
+    this.driver = driver;
+    this.drive = function(speed, time){
         console.log(speed * time);
-    },
-   logDriver: function(){
-    console.log("Driver's name is: " + this.driver);
-   }
-};
-myCar2.logDriver();
-console.log(myCar2.maxSpeed);
-myCar2.drive(50, 3);
+    };
+    this.logDriver = function(){
+        console.log("Driver's name is: " + this.driver);
+       };
+}
+var myCar = new Car(70, "Addisalem");
+var myCar2 = new Car(80, "Addis");
+var myCar3 = new Car(90, "AddisHail");
+var myCar3 = new Car(100, "Others");
+
+myCar.drive(30,5)
+myCar3.logDriver();
+
+
+
 
 
 

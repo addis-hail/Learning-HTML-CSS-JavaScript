7a6891582558035905e186f795ee43cf47fb4948
Addisalem <addishailegnaw@yahoo.com>
2022-07-17 11:39:23 +0300

    THIS keyword

--- javascript/main.js
+++ javascript/main.js
@@ -1,25 +1,14 @@
-var myArray = new Array();
-myArray[0] = 8;
-myArray[1] = "hello";
-
-var myCar = new Object();
-myCar.maxSpeed = 50;
-myCar.driver = "Addisalem";
-
-myCar.drive = function(){
-    console.log("Now driving");
-};
-
-myCar.drive();
-
 var myCar2 = {
     maxSpeed:70, 
     driver:"Addisalem", 
     drive:function(speed, time){
         console.log(speed * time);
-    }
+    },
+   logDriver: function(){
+    console.log("Driver's name is: " + this.driver);
+   }
 };
-
+myCar2.logDriver();
 console.log(myCar2.maxSpeed);
 myCar2.drive(50, 3);
 
